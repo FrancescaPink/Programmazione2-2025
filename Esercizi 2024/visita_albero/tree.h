ba1daef5317570a5177e308b23d6d88a65b03199
FrancescaPink <francescarosa2003@gmail.com>
2025-06-27 11:19:11 +0200

    Esercizi degli alberi - 2024

---
+++ Esercizi 2024/visita_albero/tree.h
@@ -0,0 +1,23 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include "tipo.h"
+
+struct node {
+   tipo_inf inf;
+   node* parent; //opzionale
+	  node* firstChild;
+	  node* nextSibling;
+};
+
+typedef node* tree;
+
+node* new_node(tipo_inf);
+void insert_child(tree, tree);
+void insert_sibling(node*, tree);
+tipo_inf get_info(node*);
+node* get_parent(node*);
+node* get_firstChild(node*);
+node* get_nextSibling(node*);
+
+#endif
